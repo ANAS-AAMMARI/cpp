630f72a516cccc25ccc5d0fb41bd816152a6df26
Anas Aammari <aaammari@e1r6p13.1337.ma>
2023-07-05 16:58:31 +0100

    day-05

---
+++ day-05/ex00/Bureaucrat.cpp
@@ -0,0 +1,84 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.cpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aaammari <aaammari@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/05 15:38:37 by aaammari          #+#    #+#             */
+/*   Updated: 2023/07/05 16:39:59 by aaammari         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat(void) : name("default"), grade(150)
+{
+}
+
+Bureaucrat::Bureaucrat(std::string const name, int grade) : name(name)
+{
+	if (grade < 1)
+		throw Bureaucrat::GradeTooHighException();
+	else if (grade > 150)
+		throw Bureaucrat::GradeTooLowException();
+	else
+		this->grade = grade;
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat &src) : name(src.name)
+{
+	*this = src;
+}
+
+Bureaucrat::~Bureaucrat(void)
+{
+}
+
+Bureaucrat &Bureaucrat::operator=(const Bureaucrat &other)
+{
+	if (this != &other)
+		this->grade = other.grade;
+	return (*this);
+}
+
+std::string const Bureaucrat::getName(void) const
+{
+	return (this->name);
+}
+
+int Bureaucrat::getGrade(void) const
+{
+	return (this->grade);
+}
+
+void Bureaucrat::incrementGrade(void)
+{
+	if (this->grade == 1)
+		throw Bureaucrat::GradeTooHighException();
+	else
+		this->grade--;
+}
+
+void Bureaucrat::decrementGrade(void)
+{
+	if (this->grade == 150)
+		throw Bureaucrat::GradeTooLowException();
+	else
+		this->grade++;
+}
+
+const char *Bureaucrat::GradeTooHighException::what() const throw()
+{
+	return ("Grade is too high");
+}
+
+const char *Bureaucrat::GradeTooLowException::what() const throw()
+{
+	return ("Grade is too low");
+}
+
+std::ostream &operator<<(std::ostream &out, const Bureaucrat &bureaucrat)
+{
+	return out << bureaucrat.getName() << ", bureaucrat grade " << bureaucrat.getGrade() << std::endl;
+}
---
+++ day-05/ex00/main.cpp
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aaammari <aaammari@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/05 15:45:23 by aaammari          #+#    #+#             */
+/*   Updated: 2023/07/05 16:55:25 by aaammari         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Bureaucrat.hpp"
+
+int main(void)
+{
+	try
+	{
+		Bureaucrat("Bureaucrat", 0);
+	}
+	catch (Bureaucrat::GradeTooHighException &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+	try
+	{
+		Bureaucrat("Bureaucrat", 151);
+	}
+	catch (Bureaucrat::GradeTooLowException &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+	try
+	{
+		Bureaucrat bureaucrat = Bureaucrat("Bureaucrat", 1);
+		bureaucrat.incrementGrade();
+		std::cout << bureaucrat << std::endl;
+		bureaucrat.decrementGrade();
+		std::cout << bureaucrat << std::endl;
+	}
+	catch (std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+	return (0);
+}
